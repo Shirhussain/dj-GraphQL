7a718051b255f70ea1db896f5a72907d0f078151
Shirhussain <sh.danishyar@gmail.com>
2021-05-06 15:36:44 +0430

    Mutation added for updated and delete

--- quiz/schema.py
+++ quiz/schema.py
@@ -35,12 +35,12 @@ class Query(graphene.ObjectType):
 
     def resolve_all_questions(root, info, id):
         return Question.objects.get(pk=id)
-    
+
     def resolve_all_answers(root, info, id):
-        # here i have foreignkey so i can call best on that and get all the answer 
+        # here i have foreignkey so i can call best on that and get all the answer
         # by just one question
         return Answer.objects.filter(question=id)
-    
+
     # quiz = graphene.String()
     # def resolve_quiz(root, info):
     #     return f'This is the first question'
@@ -80,7 +80,7 @@ class Query(graphene.ObjectType):
 
     # def resolve_all_quizzes(root, info):
     #     return Quiz.objects.all()
-    
+
     # def resolve_all_questions(root, info):
     #     return Question.objects.all()
 
@@ -95,9 +95,6 @@ class Query(graphene.ObjectType):
     # def resolve_all_quizzes(root, info):
     #     # to return specific id's or field
     #     return Quiz.objects.filter(id=2)
-
-schema = graphene.Schema(query=Query)
-
 
 
 # class BooksType(DjangoObjectType):
@@ -114,3 +111,49 @@ schema = graphene.Schema(query=Query)
 #         # return Books.objects.filter(title="django books")
 
 # schema = graphene.Schema(query=Query)
+
+
+
+# class CategoryMutation(graphene.Mutation):
+#     # in this class i can define method to save data
+#     class Arguments:
+#         name = graphene.String(required=True)
+
+#     category = graphene.Field(CategoryType)
+
+#     @classmethod
+#     def mutate(cls, root, info, name):
+#         category = Category(name=name)
+#         category.save()
+#         return CategoryMutation(category=category)
+
+
+class CategoryMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        # if you run for deleting you you need just 'id' so comment the line bellow
+        # name = graphene.String(required=True)
+
+    category = graphene.Field(CategoryType)
+
+    @classmethod
+    # def mutate(cls, root, info, name, id):
+    def mutate(cls, root, info, id):
+        category = Category.objects.get(id=id)
+        # you can update or delete form here
+        # category.name = name
+        # category.save()
+        # for deleting you don't need the name all you need to do is pass the 'id'
+        category.delete()
+        # for deleting you don't need the line bellow so i will commented
+        # return CategoryMutation(category=category)
+        return
+
+
+class Mutation(graphene.ObjectType):
+    # here we can add or update category
+    # the mutations is work like post in REST
+    update_category = CategoryMutation.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
